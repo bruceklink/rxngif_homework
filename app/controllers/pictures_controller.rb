408c5010a71742c1c6eae384c11b85a42de32457
Bruce M. Klink <b-klink@kellogg.northwestern.edu>
2013-11-07 01:06:18 -0600

    re-living class

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,7 @@
 class PicturesController < ApplicationController
   def show
+  	@id = params[:id]
+
+  	@p = Picture.find_by(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+puts "howdy"
+
 Picture.destroy_all
 
 pictures = [
